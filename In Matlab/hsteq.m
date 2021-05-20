bc3546bca2b52222d149c835cfb1ef30b806025f
KarimSakr99 <79056477+KarimSakr99@users.noreply.github.com>
2021-05-20 14:03:38 +0200

    Update hsteq.m

--- In Matlab/hsteq.m
+++ In Matlab/hsteq.m
@@ -5,13 +5,12 @@ sum=0;
 for i=1:256
     sum=sum+counts(i);
     counts(i)=sum*255/(sz(1)*sz(2));
-    counts=round(counts);
-    
+    counts=round(counts);  
 end
 
 for j=1:sz(1)
     for k=1:sz(2)
-    img(j,k)=counts(img(j,k));   
+    img(j,k)=counts(img(j,k)+1);   
     end
 end
 end
